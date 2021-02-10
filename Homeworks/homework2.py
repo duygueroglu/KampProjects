3425ab33a417e445ce32043d97109dfcde17639b
duygueroglu <45760467+duygueroglu@users.noreply.github.com>
2021-02-10 18:56:13 +0300

    homework2 added
    Second homework of the first week added

---
+++ Homeworks/homework2.py
@@ -0,0 +1,10 @@
+username= "duygueroglu"
+password="123"
+
+username=input()
+password=input()
+
+if username=="duygueroglu" and password=="123":
+  print("Welcome!")
+else:
+  print("Incorrect username or password.")
