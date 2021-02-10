f75ad2213540e9445f93a157ffb80d27169945ac
duygueroglu <45760467+duygueroglu@users.noreply.github.com>
2021-02-11 01:41:15 +0300

    degervereferanstipler files added
    files about value type and reference type added.

---
+++ DegerVeReferansTipler/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace DegerVeReferansTipler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int,decimal,float,double,bool-->değer tip - stack'te
+            //array,class,interface-->referans tip - stack ve heap'te
+
+            int sayi1 = 10;
+            int sayi2 = 30;
+            sayi1 = sayi2;
+            sayi2 = 65;
+            //sayi1 ?? 30
+
+            int[] sayilar1 = new int[] { 10, 20, 30 };
+            int[] sayilar2 = new int[] { 100, 200, 300 };
+            sayilar1 = sayilar2;
+            sayilar2[0] = 999;
+            //sayilar1[0] ?? 999
+        }
+    }
+}
