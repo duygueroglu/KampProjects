487ff3435718216b178e7ee7bd6267372275b3b6
duygueroglu <45760467+duygueroglu@users.noreply.github.com>
2021-02-14 15:55:56 +0300

    first oop files added
    first oop console app classes added

---
+++ OOP1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace OOP1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Product product1 = new Product();
+            product1.Id = 1;
+            product1.CategoryId = 2;
+            product1.ProductName = "Table";
+            product1.UnitPrice = 500;
+            product1.UnitsInStock = 3;
+
+            Product product2 = new Product
+            {
+                Id = 2,
+                CategoryId = 5,
+                UnitsInStock = 5,
+                ProductName = "Pencil",
+                UnitPrice = 35
+            };
+            //stack                             //heap
+            ProductManager productManager = new ProductManager();
+            productManager.Add(product1);
+            Console.WriteLine(product1.ProductName);
+
+            //int, double, bool...value type
+            //array, class, abstract class, interface... reference type
+            //ref out
+        }
+    }
+}
