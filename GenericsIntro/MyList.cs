bacb17945676e0c47ce7402a58525c40ad9cafea
duygueroglu <45760467+duygueroglu@users.noreply.github.com>
2021-02-15 16:43:23 +0300

    generics file added
    files about generic classes sample added

---
+++ GenericsIntro/MyList.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericsIntro
+{
+    class MyList<T>
+    {
+        T[] items;
+
+        //keep in memory
+        //constructor, works automatically when class renewed
+        public MyList()
+        {
+            //array created with 0 element
+            items = new T[0];
+
+        }
+        public void Add(T item)
+        {
+            T[] tempArray = items; //tempArray, keep items's reference
+            items = new T[items.Length + 1]; // new array created
+            for (int i = 0; i < tempArray.Length; i++)
+            {
+                items[i] = tempArray[i];
+            }
+            //last added element
+            items[items.Length - 1] = item;
+        }
+        public int Length
+        {
+            get { return items.Length; }
+        }
+
+        public T[] Items
+        {
+            get { return items; }
+        }
+    }
+}
+    
---
+++ GenericsIntro/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace GenericsIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyList<string> names = new MyList<string>(); //create reference in memory
+            
+            names.Add("Duygu");
+            Console.WriteLine(names.Length);
+            names.Add("Dilek");
+            Console.WriteLine(names.Length);
+
+            foreach (var name in names.Items)
+            {
+                Console.WriteLine(name);
+            }
+        }
+    }
+}
