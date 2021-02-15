74ba2602b2f10196f5f3536e65be632eb1eb37bd
duygueroglu <45760467+duygueroglu@users.noreply.github.com>
2021-02-15 17:12:24 +0300

    MyDictionary homework added
    MyDictionary homework files added.

---
+++ Homeworks/HomeworkDictionary/MyDictionary.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HomeworkDictionary
+{
+    class MyDictionary<Key,Value>
+    {
+        List<Key> keys;
+        List<Value> values;
+
+        public MyDictionary()
+        { 
+            values = new List<Value>();
+            keys = new List<Key>();
+        }
+
+        public void Add(Key key, Value value)
+        {
+            keys.Add(key);
+            values.Add(value);
+        }
+
+        public void Keys(Key key)
+        {
+            try
+            {
+                Console.WriteLine(values[keys.IndexOf(key)]);
+            }
+            catch
+            {
+                Console.WriteLine("Word doesn't exist.");
+            }
+        }
+    }
+}
---
+++ Homeworks/HomeworkDictionary/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace HomeworkDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> myDictionary = new MyDictionary<int, string>();
+            myDictionary.Add(1, "tree");
+            myDictionary.Add(2, "apple");
+            myDictionary.Add(3, "hungry");
+
+            myDictionary.Keys(1);
+            myDictionary.Keys(3);
+            myDictionary.Keys(19);
+        }
+    }
+}
