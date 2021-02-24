d92317b1f1a765f45df53fd8b7eff913475c38db
duygueroglu <45760467+duygueroglu@users.noreply.github.com>
2021-02-24 18:38:49 +0300

    program file added

---
+++ OOP3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace OOP3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //interfaces can keep reference of the class that implement it.
+
+            ICreditManager personalFinanceCreditManager = new PersonalFinanceCreditManager();
+            ICreditManager transportCreditManager = new TransportCreditManager();
+            ICreditManager mortgageCreditManager = new MortgageCreditManager();
+
+            ILoggerService databaseLoggerService = new DatabaseLoggerService();
+            ILoggerService fileLoggerService = new FileLoggerService();
+            
+            ApplicationManager applicationManager = new ApplicationManager();
+            applicationManager.Apply(new ArtisanCreditManager(), databaseLoggerService);
+
+            List<ICreditManager> credits = new List<ICreditManager>() { personalFinanceCreditManager, transportCreditManager};
+            //applicationManager.MakeCreditPreNotification(credits);
+            
+        }
+    }
+}
