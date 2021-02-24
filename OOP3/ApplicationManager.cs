9f346d4518d58cc7a32f83cb445a6088495b8969
duygueroglu <45760467+duygueroglu@users.noreply.github.com>
2021-02-24 18:38:38 +0300

    application manager added

---
+++ OOP3/ApplicationManager.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP3
+{
+    class ApplicationManager
+    {
+        //başvur
+        //Method injection
+        public void Apply(ICreditManager creditManager, ILoggerService loggerService)
+        {
+            creditManager.Calculate();
+            loggerService.Log();
+
+        }
+
+        //kredi ön bilgilendirmesi yap
+        public void MakeCreditPreNotification(List<ICreditManager> credits)
+        {
+            foreach (var credit in credits)
+            {
+                credit.Calculate();
+            }
+        }
+    }
+}
